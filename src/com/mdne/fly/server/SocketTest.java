53ee06aaf396a14a0112ce369176b17486b2933d
Chaos <kuzper@gmail.com>
2011-01-26 06:59:41 +0000

    tryed to use server's flash back

--- src/com/mdne/fly/server/SocketTest.java
+++ src/com/mdne/fly/server/SocketTest.java
@@ -2,6 +2,7 @@ package com.mdne.fly.server;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
@@ -11,17 +12,21 @@ public class SocketTest {
 	private static byte[] tmp = new byte[4];
 	private static float[] floatTmp = new float[3];
 	private static boolean flag;
+	private static byte[] output = new byte [1] ;
 
 	public static void main(String[] args) throws InterruptedException {
 		while (true) {
 			try {
 				ServerSocket s = new ServerSocket(64445);
 				Socket incomming = s.accept();
-				InputStream dis = incomming.getInputStream();
+				InputStream is = incomming.getInputStream();
+				OutputStream os = incomming.getOutputStream();
 				flag = false;
+//				output[0] =  Byte.valueOf("11", 16);
+//				os.write(output);
 
 				while (!flag) {
-					dis.read(byteArray);
+					is.read(byteArray);
 					ByteBuffer bb = ByteBuffer.allocate(4);
 					for (int i = 0; i < 3; i++) {
 						int n = i * 4;
@@ -37,11 +42,12 @@ public class SocketTest {
 						System.out.println(f);
 					}
 					System.out.println("\n");
+					
 					if (Byte.valueOf("11", 16).equals(byteArray[12])) {
 						flag = true;
 					}
 				}
-				dis.close();
+				is.close();
 				s.close();
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
