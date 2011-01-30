823e46fe1c6c5fcc1f20db4d47996e62914656b6
Chaos <kuzper@gmail.com>
2011-01-30 10:59:18 +0000

    commented OutputStream

--- src/com/mdne/fly/server/SocketTest.java
+++ src/com/mdne/fly/server/SocketTest.java
@@ -2,7 +2,7 @@ package com.mdne.fly.server;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.OutputStream;
+//import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
